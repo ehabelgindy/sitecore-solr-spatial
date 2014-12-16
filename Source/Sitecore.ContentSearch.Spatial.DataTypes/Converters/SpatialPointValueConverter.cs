876b01cadbbef8a24deeb25179f67f15f43d78ff
Ehab ElGindy <ehabelgindy@hotmail.com>
2014-12-16 14:53:49 +0000

    adding spatial point value converter

---
+++ Source/Sitecore.ContentSearch.Spatial.DataTypes/Converters/SpatialPointValueConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.ContentSearch.Spatial.DataTypes.Converters
+{
+    public class SpatialPointValueConverter : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if (sourceType == typeof(SpatialPoint))
+                return true;
+            else
+                return base.CanConvertFrom(context, sourceType);
+        }
+
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+        {
+            if (destinationType == typeof(string))
+                return true;
+            else
+                return base.CanConvertTo(context, destinationType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if ((value is string))
+                return new SpatialPoint((string)value);
+            return new SpatialPoint();
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            return ((SpatialPoint)value).ToString();
+        }
+    }
+}
