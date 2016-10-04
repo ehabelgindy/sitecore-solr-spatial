20863e2468b92e7e1f91febfd6475053a8e60ba7
James Forage <james@yoyodesign.com>
2016-10-04 11:55:08 +0100

    Implementing SwitchOnRebuild index provider option

---
+++ Source/Sitecore.ContentSearch.Spatial.Solr/Provider/SwitchOnRebuildSolrSearchIndexWithSpatial.cs
@@ -0,0 +1,17 @@
+﻿using Sitecore.ContentSearch.Maintenance;
+using Sitecore.ContentSearch.SolrProvider;
+
+namespace Sitecore.ContentSearch.Spatial.Solr.Provider
+{
+    public class SwitchOnRebuildSolrSearchIndexWithSpatial : SwitchOnRebuildSolrSearchIndex
+	{
+        public SwitchOnRebuildSolrSearchIndexWithSpatial(string name, string core, string rebuildcore, IIndexPropertyStore propertyStore) : base(name, core, rebuildcore, propertyStore)
+        {
+        }
+
+        public override IProviderSearchContext CreateSearchContext(Security.SearchSecurityOptions options = Security.SearchSecurityOptions.EnableSecurityCheck)
+        {
+            return new SolrSearchWithSpatialContext(this,options);
+        }
+    }
+}
